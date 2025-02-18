263ecb2e90574094aa571493e644679a308875f6
chaeyounglim <limcy423@gmail.com>
2025-02-18 01:27:42 +0100

    first commit

---
+++ src/main/java/com/goorm/liargame/LiargameApplication.java
@@ -0,0 +1,15 @@
+package com.goorm.liargame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class LiargameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiargameApplication.class, args);
+	}
+
+}
